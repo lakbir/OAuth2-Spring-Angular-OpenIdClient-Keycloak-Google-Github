e6c6189cc2db55de2c4f7a2e03d897c09e7beb72
lakbir <lakbir1993@gmail.com>
2023-12-24 21:32:04 +0100

    connexion keycloak, profile user et gestion roles

--- customer-front-angular-app/src/app/app.component.ts
+++ customer-front-angular-app/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {SecurityService} from "./services/security.service";
+import {KeycloakService} from "keycloak-angular";
+import {KeycloakProfile} from "keycloak-js";
 
 @Component({
   selector: 'app-root',
@@ -8,19 +10,24 @@ import {SecurityService} from "./services/security.service";
 })
 export class AppComponent implements OnInit{
   title = 'customer-front-angular-app';
-  constructor(public securityService : SecurityService) {
+  public profile? : KeycloakProfile;
+  constructor(public keycloakService: KeycloakService) {
   }
   ngOnInit() {
-    console.log(this.securityService.profile)
+    if(this.keycloakService.isLoggedIn()){
+      this.keycloakService.loadUserProfile().then(profile=>{
+        this.profile=profile;
+      });
+    }
   }
 
   async login(){
-    await this.securityService.kcService.login({
+    await this.keycloakService.login({
       redirectUri : window.location.origin
     })
   }
   onLogout() {
-    this.securityService.kcService.logout(window.location.origin);
+    this.keycloakService.logout(window.location.origin);
   }
   account() {
     window.location.href="http://localhost:8080/realms/ebank-realm/account/#/personal-info";
